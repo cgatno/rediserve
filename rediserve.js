33af2399107b3f99cfa721f2eda5a182cc7786c4
Christian Gaetano <get@christiangaeta.no>
2016-10-25 11:44:00 -0400

    Add very basic server logic for testing

--- rediserve.js
+++ rediserve.js
@@ -5,8 +5,37 @@
 
 var rediserve = function () {};
 
-rediserve.prototype.up = function(redisCli, expressApp) {
+/**
+ * Initialize a rediserve web server listening on port 8080 using provided Express routing and Redis client connection. No additional configuration can be performed at this time.
+ * 
+ * TODO: Add configuration options!
+ * 
+ * @param {Object} redisCli - An initialized Redis client connection from node_redis (https://github.com/NodeRedis/node_redis)
+ * @param {Object} expressApp - An initialized ExpressJS app object (e.g. var app = express();). See ExpressJS documentation for more information (http://expressjs.com/)
+ * @param {string} appName - A string representation of the app name. The server assumes that this is also a prefix for keys in your Redis database (i.e. appName:index:current) 
+ */
+rediserve.prototype.up = function (redisCli, expressApp, appName) {
+    expressApp.get('*', function (req, res) {
+        redisCli.get(appName + ':index:current-content'),
+            function (err, value) {
+                if (!err) {
+                    let html = value;
+                    res.send(html);
+                } else {
+                    throw new rediserve.RedisGetException(err);
+                }
+            }
+    });
+}
 
+/**
+ * Defines a specialized exception for Redis GET errors
+ * 
+ * @param {string} message - A message explaining (in detail!) the cause of a Redis GET error
+ */
+rediserve.prototype.RedisGetException = function (message) {
+    this.message = message;
+    this.name = "RedisGetException";
 }
 
 module.exports = new rediserve();
