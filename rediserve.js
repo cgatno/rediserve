e6012bd717c4cb439e9c6aa073c0cbc6de12711e
Christian Gaetano <Christian.Gaetano@componentone.com>
2016-10-25 11:13:08 -0400

    Use strict mode for rediserve module

--- rediserve.js
+++ rediserve.js
@@ -1,10 +1,12 @@
+'use strict';
+
 // rediserve.js
 /* rediserve is a custom nodejs web server designed to be used in conjunction with the Redis-based storage of the Ember Lightning Fast Deployment methodology. The server leverages ExpressJS to quickly serve up current index.html markup while allowing developers and QA teams to easily view specific revisions of index.html before making them public. */
 
 var rediserve = function () {};
 
 rediserve.prototype.up = function(redisCli, expressApp) {
-    
+
 }
 
 module.exports = new rediserve();
